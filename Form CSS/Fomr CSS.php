8a043d830afe9b55480689b55f5f730bf109465d
Ratana667 <ratana667@gmial.com>
2021-01-09 15:08:58 +0700

    update css

---
+++ Form CSS/Fomr CSS.php
@@ -0,0 +1,22 @@
+<html>
+
+
+
+First Name:<?php echo $_POST["Firstname"];?><br>
+
+Last Name:<?php echo $_POST["Lastname"];?><br>
+
+Country:<?php echo $_POST["Country"];?><br>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
